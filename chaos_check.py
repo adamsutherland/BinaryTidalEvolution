b88fcaae278b75e76e68d1236f40ff045adf06fa
Adam Sutherland <adamsutherland@uchicago.edu>
2019-07-08 17:08:57 -0700

    Speed up chaos check
    Reduces number of redundant calculations

--- chaos_check.py
+++ chaos_check.py
@@ -22,6 +22,31 @@ csv = csv[(csv.Name == "34b")]
 #csv = csv[(csv.Name != "16b")]
 #csv = csv[(csv.Name != "35b")]
 
+def mmm_factors(mp,ms,d):
+    f1 = 45*mp*ms*(mp**2-mp*ms+ms**2)*d**4
+    f2 = 48*mp*ms*(mp+ms)**2*d**2
+    f3 = 64*(mp+ms)**4
+    f4 = 64*(mp+ms)**3
+    return f1, f2, f3, f4
+
+def mmm_fast(f1,f2,f3,f4,a):
+    n2 = qs.G*(f1 + f2*a**2 + f3*a**4)/(f4 * a**7)
+    return n2**.5
+
+def mod_a_from_n(mp, ms, d, n,f1,f2,f3,f4):
+    """Finds sma from mean motion, n"""
+    a=qs.sma(mp,ms,2*np.pi/n)/2
+    for x in xrange(16):
+        r=0.0
+        count = 0
+        while (r < 1):
+            count +=1
+            a = a + 10**-x
+            r =  n/mmm_fast(f1,f2,f3,f4,a)
+            if count > 100:
+                break
+        a=a-10**-x
+    return a
 
 def check(m1,m2,d,ei,eo,ap):
     n1 = qs.mean_mo(m1,m2,d)
@@ -58,8 +83,46 @@ def check(m1,m2,d,ei,eo,ap):
             inside = 0
     return inside#, a
 
+def check_fast(m1,m2,d,ei,eo,ap):
+    f1, f2, f3, f4 = mmm_factors(m1,m2,d)
+    n1 = qs.mean_mo(m1,m2,d)
+    n = int(n1/mmm_fast(f1,f2,f3,f4,ap))
+    width = qs.sigma_mard(m1,m2,0.,eo,ei,n)
+    a = mod_a_from_n(m1,m2,d,n1/(n),f1,f2,f3,f4)
+    wdot = mmm_fast(f1,f2,f3,f4,a)-qs.mod_epic(m1,m2,d,a)
+    wdot *=(1+1.5*ei**2)
+    n2 = (n1+(n-1)*wdot)/(n)
+    #print qs.mod_a_from_n(m1,m2,d,n1/(width+n1/n2))
+    #print qs.mod_a_from_n(m1,m2,d,n1/(-width+n1/n2))
+    if ap > mod_a_from_n(m1,m2,d,n1/(-width+n1/n2),f1,f2,f3,f4):
+        if ap < mod_a_from_n(m1,m2,d,n1/(width+n1/n2),f1,f2,f3,f4):
+            inside =  n
+        else:
+            inside = 0
+    else:
+        inside = 0
+    if inside < 1:
+        n +=1
+        width = qs.sigma_mard(m1,m2,0.,eo,ei,n)
+        a = mod_a_from_n(m1,m2,d,n1/(n),f1,f2,f3,f4)
+        wdot = mmm_fast(f1,f2,f3,f4,a)-qs.mod_epic(m1,m2,d,a)
+        wdot *=(1+1.5*ei**2)
+        n2 = (n1+(n-1)*wdot)/(n)
+        #print qs.mod_a_from_n(m1,m2,d,n1/(width+n1/n2))
+        #print qs.mod_a_from_n(m1,m2,d,n1/(-width+n1/n2))
+        if ap > mod_a_from_n(m1,m2,d,n1/(-width+n1/n2),f1,f2,f3,f4):
+            if ap < mod_a_from_n(m1,m2,d,n1/(width+n1/n2),f1,f2,f3,f4):
+                inside =  n
+            else:
+                inside = 0
+        else:
+            inside = 0
+    return inside#, a
+
 ftide = "f30"
 
+
+baseline = 1.5
 
 def check_row(row):
     print row["Name"]
@@ -79,10 +142,10 @@ def check_p(row):
     df = pd.read_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
     chaos = np.array([])
     for index2, row2 in df.iterrows():
-        ap = 1.7
+        ap = qs.sma(row["m1"],row["m2"],365*baseline)
         ep = row2["e"]*row2["a"]/ap*(row["m1"]-row["m2"])/(row["m1"]+row["m2"])/0.4115
         #ep = row["ep"]
-        chaos = np.append(chaos,check(row["m1"],row["m2"],row2["a"],row2["e"],ep,ap))
+        chaos = np.append(chaos,check_fast(row["m1"],row["m2"],row2["a"],row2["e"],ep,ap))
     df["chaos"] = chaos
     print chaos.max()
     df.to_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
@@ -91,13 +154,13 @@ import multiprocessing as mp
 
 systems = qs.pd.read_csv("../../Projects/tidal/popsynth/systems.txt", names=["Name","m1","m2","abin","ebin","R1","R2","L1","L2"])
 
-systems = systems[systems.Name<3]
-systems = systems[systems.Name>1]
+systems = systems[systems.Name>408]
+systems = systems[systems.Name<410]
 
 
 numcpu = mp.cpu_count()
 rows = [row for index, row in systems.iterrows()]
-#
+
 #if len(rows) % numcpu != 0:
 #    batches = len(rows)/numcpu+1
 #    numcpu = len(rows)/batches+1
@@ -111,11 +174,11 @@ for row in rows:
     df = pd.read_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
     chaos = np.array([])
     for index2, row2 in df.iterrows():
-        ap = 1.7
+        ap = ap = qs.sma(row["m1"],row["m2"],365*baseline)
         ep = row2["e"]*row2["a"]/ap*(row["m1"]-row["m2"])/(row["m1"]+row["m2"])/0.4115
-        print row2["a"]
+        #print row2["a"]
         #ep = row["ep"]
-        chaos = np.append(chaos,check(row["m1"],row["m2"],row2["a"],row2["e"],ep,ap))
+        chaos = np.append(chaos,check_fast(row["m1"],row["m2"],row2["a"],row2["e"],ep,ap))
     df["chaos"] = chaos
     print chaos.max()
     df.to_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
--- plot.py
+++ plot.py
@@ -17,7 +17,6 @@ def hwac(e,mu):
 
 csv = pd.read_csv("kepler_data.csv",delim_whitespace=True)
 csv = csv[(csv.Name != "47d")&(csv.Name != "47c")]
-plt.figure()
  
 
 systems = pd.read_csv("../../Projects/tidal/popsynth/systems.txt", names=["Name","m1","m2","abin","ebin","R1","R2","L1","L2"])
@@ -33,45 +32,56 @@ runs = glob.glob("../../Projects/tidal/popsynth/"+ftide+"/*.p")
 
 #runs = runs[::11]
 
+baseline = 0.5
+#baseline = 1.0
+#baseline = 1.5
 
-for run in runs:
-    name =  int(run[-7:-2])
-    m1 = systems.iloc[name].m1
-    m2 = systems.iloc[name].m2
-    mu = m2/(m1 + m2)
-    #Name = str(int(row['Name'])).zfill(5)
-    #print Name
-    #df = pd.read_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
-    df = pd.read_pickle(run)
-    a = df.iloc[-1].a
-    e = df.iloc[-1].e
-    hw = hwac(e,mu)
-    ap = qs.sma(m1,m2,365*1.5)
-    #a0, eo = df.iloc[0].a, df.iloc[0].e
-    a0, e0 = systems.iloc[name].abin, systems.iloc[name].ebin
-    if a*hw > ap:
-        #print "Unstable"
-        plt.plot(a0,e0,"x",c="red", alpha=0.5)
-    else:
-        plt.plot(a0,e0,".",c="blue", alpha=0.5)
+baselines = [0.5,1.0,1.5]
 
-kepler = [row1 for index1, row1 in csv.iterrows()]
-for kep in kepler:
-    plt.plot(kep.abin,kep.ebin,"o",c="orange",)
-    plt.text(kep.abin,kep.ebin,kep.Name)
-
-plt.ylim(0,.65)
-plt.xlim([0.025,.25])
-plt.xlabel("$a_{bin}$")
-plt.ylabel("$e_{bin}$")
-plt.show()
-
-plt.figure()
-for run in runs:
-    df = pd.read_pickle(run)
-    plt.plot(df.a,df.e,alpha=0.5)
-plt.ylim(0,1)
-plt.xlim([0.025,.35])
-plt.xlabel("$a_{bin}$")
-plt.ylabel("$e_{bin}$")
-plt.show()
+for baseline in baselines:
+    plt.figure()
+    for run in runs:
+        name =  int(run[-7:-2])
+        m1 = systems.iloc[name].m1
+        m2 = systems.iloc[name].m2
+        mu = m2/(m1 + m2)
+        #Name = str(int(row['Name'])).zfill(5)
+        #print Name
+        #df = pd.read_pickle("../../Projects/tidal/popsynth/"+ftide+"/"+Name+".p")
+        df = pd.read_pickle(run)
+        a = df.iloc[-1].a
+        e = df.iloc[-1].e
+        hw = hwac(e,mu)
+        ap = qs.sma(m1,m2,365*baseline)
+        #a0, eo = df.iloc[0].a, df.iloc[0].e
+        a0, e0 = systems.iloc[name].abin, systems.iloc[name].ebin
+        p0 = qs.period(m1,m2,a0)
+        if a*hw > ap:
+            #print "Unstable"
+            plt.plot(p0,e0,"x",c="red", alpha=0.5)
+        else:
+            plt.plot(p0,e0,".",c="blue", alpha=0.5)
+    
+    kepler = [row1 for index1, row1 in csv.iterrows()]
+    for kep in kepler:
+        plt.plot(kep.Pbin,kep.ebin,"o",c="orange",)
+        plt.text(kep.Pbin,kep.ebin,kep.Name)
+    
+    plt.ylim(0,.65)
+    #plt.xlim([0.025,.25])
+    plt.xlabel("$P_{bin}$ (days)")
+    plt.ylabel("$e_{bin}$")
+    #plt.show()
+    plt.title("Max Period = "+str(baseline))
+    plt.savefig("/Users/adam/Projects/tidal/popsynth/stable"+str(baseline)+"_p.png")
+    
+    #plt.figure()
+    #for run in runs:
+    #    df = pd.read_pickle(run)
+    #    plt.plot(df.a,df.e,alpha=0.5)
+    #plt.ylim(0,1)
+    #plt.xlim([0.025,.35])
+    #plt.xlabel("$a_{bin}$")
+    #plt.ylabel("$e_{bin}$")
+    #plt.show()
+    plt.close()
